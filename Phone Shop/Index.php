f96a9cbecd146f67ca2f90e65f7702f0eb91b108
teamweb7 <91516024+NguyEnWBIE@users.noreply.github.com>
2024-02-24 18:22:55 +0700

    NEW

---
+++ Phone Shop/Index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/Template.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="page">
+        <?php
+        require("PAGES/TopMenu.php");
+        ?>
+        <?php
+        require("PAGES/Content.php");
+        ?>
+    </div>
+    <?php
+    require("PAGES/Footer.php");
+    ?>
+</body>
+
+</html>
---
+++ Phone Shop/PAGES/Footer.php
@@ -0,0 +1,47 @@
+<div class="myFooter">
+    <div class="container_Col_myFooter">
+        <div class="col1">
+            <div style="height: 50px;" class="row_OfCol">
+                <img style="cursor:pointer" src="https://www.themobileshop.ca/static/946dd4651eb22e4ad0ecdf21d563a334/TMS-Logo_White_New-Tagline_EN.svg" alt="">
+            </div>
+            <div style="height: 50px;" class="row_OfCol">
+                <img style="width: 10%; cursor:pointer" src="https://www.themobileshop.ca/static/b828f06a08f40b74290399eb618f793d/instagram_logo.svg" alt="">
+                <img style="width: 10%; cursor:pointer" src="https://www.themobileshop.ca/static/c10bc8b22c431cae5c36895ca68e8f93/facebook_logo.svg" alt="">
+                <img style="width: 10%; cursor:pointer" src="https://www.themobileshop.ca/static/48704f9f9a8d1fed22059f080f056da7/messenger_logo.svg" alt="">
+            </div>
+        </div>
+        <div class="col2">
+            <div class="row_OfCol">
+                <a href="">
+                    Blog</a>
+            </div>
+            <div class="row_OfCol">
+                <a href="">Price match guarantee</a>
+            </div>
+            <div class="row_OfCol">
+                <a href="">Device protection plan</a>
+            </div>
+            <div class="row_OfCol">
+                <a href="">Trade in program</a>
+            </div>
+            <div class="row_OfCol">
+                <a href="">FAQs</a>
+            </div>
+        </div>
+        <div class="col2">
+            <div class="row_OfCol">
+                <a href="">
+                    Connect with us</a>
+            </div>
+            <div class="row_OfCol">
+                <a href="">Careers</a>
+            </div>
+            <div class="row_OfCol">
+                <a href="">Privacy policy</a>
+            </div>
+            <div class="row_OfCol">
+                <a href="">Terms & conditions</a>
+            </div>
+        </div>
+    </div>
+</div>
