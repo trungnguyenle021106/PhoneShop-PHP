722735697e7d6b6122816c065042f820be80f533
huyhuy1712 <147235349+huyhuy1712@users.noreply.github.com>
2024-03-02 07:25:32 +0700

    Add files via upload

---
+++ accessoriess/accessories.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/3918fe69ba.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="accessories.css">
+    <title>Accessories</title>
+</head>
+
+<body>
+        <?php
+        require("accessories_TopMenu.php");
+        ?>
+    <div class="page">
+        <?php
+        require("accessories_Content.php");
+        ?>
+    </div>
+    <?php
+    require("accessories_Footer.php");
+    ?>
+<script src="accessories.js"></script>
+</body>
+</html>
