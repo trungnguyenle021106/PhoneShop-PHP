b08ff5b3945c61c1a812c2b7efef92c358b3f63f
Nguyen <clonecobth@gmail.com>
2024-03-09 11:09:08 +0700

    sua gio hang html

--- ShopingCart.php
+++ ShopingCart.php
@@ -22,19 +22,54 @@
                         <button name="cong" id="" >+</button>
                     </div>
                     <div style="display: block;">
-                    <div class="kiemtra">
-                        <img src="https://www.themobileshop.ca/static/bccef72a0d7554f6b1b5c608d4b4d5c3/icon_notification_available.svg" alt="">
-                        <div>Available to reserve in selected store</div>
-                    </div  >
-                    <div style="width: 100%; margin-top: 20px;">
-                        <a  class="btnXoa" href="#">Xoa</a>
+                        <div class="kiemtra">
+                            <img src="https://www.themobileshop.ca/static/bccef72a0d7554f6b1b5c608d4b4d5c3/icon_notification_available.svg" alt="">
+                            <div>Available to reserve in selected store</div>
+                        </div  >
+                        <div style="width: 100%; margin-top: 20px;">
+                            <a  class="btnXoa" href="#">Xoa</a>
+                        </div>
                     </div>
-                </div>
                 </div>
             </div>
 
 
         </div>
+        
+        <div class="CartItem">
+            
+            <div class="product_image">
+                <img src="https://amsprod.blob.core.windows.net/assets/e8ddb3b8-f0e6-43eb-a9af-0ec02d34e353.png" alt="Bộ sạc Hypergear 20W">
+            </div>
+            <div class="product_detail">
+                <input type="hidden" name="product_id" id="product_id" value="">
+                <div class="Ten_sp">Accessories</div>
+                <div class="thongtinsp" >
+                    <div style="width: 50%;"><a class="thea"  href="#">Hypergear 20W White USB-C PD Wall Charger Hub</a></div>
+                    <div style="font-weight: bold;">$24.95</div>
+                </div>
+
+                <div style="display: inline-flex; width: 100%; margin-top: 30px;">
+                    <div class="soluong">
+                        <button name="tru" id="" >-</button>
+                        <input readonly type="text" name="soluong" id="" value="1">
+                        <button name="cong" id="" >+</button>
+                    </div>
+                    <div style="display: block;">
+                        <div class="kiemtra">
+                            <img src="https://www.themobileshop.ca/static/bccef72a0d7554f6b1b5c608d4b4d5c3/icon_notification_available.svg" alt="">
+                            <div>Available to reserve in selected store</div>
+                        </div  >
+                        <div style="width: 100%; margin-top: 20px;">
+                            <a  class="btnXoa" href="#">Xoa</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+        
     </div>
 
     <div class="right">
