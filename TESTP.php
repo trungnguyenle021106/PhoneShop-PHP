93121cc0b5e50e611b431931830197be66f6f29a
teamweb7 <91516024+NguyEnWBIE@users.noreply.github.com>
2024-04-14 08:06:26 +0700

    DANG PHAN TRANG

---
+++ TESTP.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body onload="LoadDataForPage(1)">
+
+    <div id="container">
+    <ul id="elementPage">
+
+    </ul>
+    <div id="pageContainer"></div>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="./js/page.js"></script>
+</html>
