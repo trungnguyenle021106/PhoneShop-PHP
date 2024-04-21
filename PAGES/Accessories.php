6c4819bb18db7318a087f747a069d4d7bc63b2cb
teamweb7 <91516024+NguyEnWBIE@users.noreply.github.com>
2024-04-21 10:48:38 +0700

    NEW

--- PAGES/Accessories.php
+++ PAGES/Accessories.php
@@ -1,19 +1,22 @@
-
-
 <div class="accessories_content">
 
         <div class="filter">
-                <div class="search">
-                        <i class="fa-solid fa-magnifying-glass" id="icon-search"></i>
-                        <input placeholder="Search for your phone"></input>
-                        <i class="fa-solid fa-chevron-down" id="arrow"></i>
-                </div>
-                <div id="type" class="item-filter">Type
-                        <i class="fa-solid fa-chevron-down" id="arrow"></i>
-                </div>
-                <div id="color" class="item-filter">Color
-                        <i class="fa-solid fa-chevron-down" id="arrow"></i>
-                </div>
+
+
+                <select class="item-filter" id="loai">
+                        <option value="Loai">LOẠI</option>
+                        <option value="Tai nghe">TAI NGHE</option>
+                        <option value="Sac">SẠC</option>
+                        <option value="Op lung">ỐP LƯNG</option>
+                </select>
+
+                <select class="item-filter" id="nsx">
+                        <option value="NSX">THƯƠNG HIỆU</option>
+                        <option value="Apple">APPLE</option>
+                        <option value="Samsung">SAMSUNG</option>
+                        <option value="Google">GOOGLE</option>
+                        <option value="Oppo">OPPO</option>
+                </select>
                 <div id="brand" class="item-filter">Brand
                         <i class="fa-solid fa-chevron-down" id="arrow"></i>
                 </div>
@@ -25,7 +28,7 @@
                 </div>
                 <button id="btn-clear"> Clear all </button>
         </div>
-        <div class="accessories_section" id="accessories_page" >
+        <div class="accessories_section" id="accessories_page">
                 <a href="" class="products">
                         <div class="name">Hypergear</div>
                         <div class="discrible">Hypergear 20W White USB-C PD Wall Charger Hub</div>
@@ -40,16 +43,17 @@
         </div>
         <div class="change_slide_content">
                 <div class="change_slide" id="Pagination">
-                        <button id="backPage_Btn"><<</button>
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button id="nextPage_Btn">>></button>
+                        <button id="backPage_Btn">
+                                <<< /button>
+                                        <button>1</button>
+                                        <button>2</button>
+                                        <button>3</button>
+                                        <button id="nextPage_Btn">>></button>
                 </div>
         </div>
         <div class="footer_content">
                 <h2 style="text-align: center; font-size: 20px;">Latest & Featured</h2>
-                <div class="accessories_section" >
+                <div class="accessories_section">
                         <a href="" class="products" style="border: 1px solid black;">
                                 <div class="name">Zagg</div>
                                 <div class="discrible">iPhone 15/14/13 ZAGG (GEAR4) Crystal Palace Case</div>
@@ -110,11 +114,9 @@
 <script src="/js/pageHandler.js"></script>
 
 <script>
-
-window.onload = function() {
-        loadData(1, 3, 3)
-    }
-
+        window.onload = function() {
+                loadData(1, 3, 3)
+        }
 </script>
 <!-- <div class="scroll_down_btn">
                 <i class="fa-solid fa-arrow-down" style="color:white; margin-left: 20px; margin-top: 5px; border-radius: 50%; background-color: red; padding: 10px;"></i>
