d04e36bf9221f85c95eadd4aa0baaaa2867b902b
NHNam2003 <nhnam4411.com>
2024-05-08 10:15:06 +0700

    inboice

--- PAGES/handleInvoice.php
+++ PAGES/handleInvoice.php
@@ -4,11 +4,29 @@
     $conn->connectDB();
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if(isset($_POST['product_id']) && isset($_POST['soluong'])) {
+            $sql1 = "hoa_don ORDER BY MA_HD DESC";
+            $order = $conn->read($sql1);
+            $mahd = $order[0]["MA_HD"] + 1;
+            $makm = 1;
+            $ngayTao = "2024-04-30";
+            $tinhTrang = "đang";
+            $manv = 1;
+            $makh = 1;
+            $tongTien = 5000000;
+            $sql2 = "INSERT INTO hoa_don (MA_HD,MA_KM,NGAY_TAO,TINH_TRANG,MA_NV,MA_KH,TONG_TIEN) VALUES ('$mahd','$makm','$ngayTao','$tinhTrang','$manv','$makh','$tongTien')";
+            if ($conn->query($sql2) === TRUE) {
+            } else {
+                if (isset($_SERVER['HTTP_REFERER'])) {
+                    echo 'loi';
+                }
+            }
+
+
             $ids = $_POST['product_id'];
             $quantities = $_POST['soluong'];
-            $mahd = 1;
             $thue = 100;
             $thanhtien = 500;
+
             for($i = 0; $i < count($ids); $i++) {
                 $sql = "INSERT INTO chi_tiet_hoadon (MA_SP,MA_HD,SL_BAN,THUE_SUAT,THANH_TIEN) VALUES ('$ids[$i]','$mahd','$quantities[$i]','$thue','$thanhtien')";
                 echo $sql . "<br>";
